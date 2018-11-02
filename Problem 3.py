2ca0bb4f913a1886f217ae085dd9fb5ad0c27a8e
Christian Agbayani Serrano <christianserrano64@gmail.com>
2018-11-01 23:08:37 -0700

    Add files via upload

---
+++ Problem 3.py
@@ -0,0 +1,34 @@
+import graphics
+from graphics import*
+import time
+from time import*
+def set_value(name, value):
+    name = value
+def set_vertex(name, x, y):
+    name = [x,y]
+def get_value(name):
+    return name
+def get_all():
+    return {a1, a2, a3, a4, a5, a6, a7, a8, s1, s2, s3, s4, s5, s6, s7, s8, ar1, ar2}
+def main():
+    a1 = 0.0
+    a2 = 0.0
+    a3 = 0.0
+    a4 = 0.0
+    a5 = 0.0
+    a6 = 0.0
+    a7 = 0.0
+    a8 = 0.0
+    s1 = 0.0
+    s2 = 0.0
+    s3 = 0.0
+    s4 = 0.0
+    s5 = 0.0
+    s6 = 0.0
+    s7 = 0.0
+    s8 = 0.0
+    ar1 = 0.0
+    ar2 = 0.0
+    win = GraphWin("Problem Set R #3")
+    win.close()
+main()
